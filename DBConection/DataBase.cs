c0c5237017df9a570a4153c27392e7dc5df9c626
Nikita Averyanov <n.s.averyanov@gmail.com>
2019-11-14 18:30:57 +0500

    Db connection

---
+++ DBConection/DataBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBConection
+{
+    public class DataBase
+    {
+        private readonly string _connectionString = @"Data Source=localhost;Initial Catalog=udb_Аверьянов_Лощенко;User Id=sa; Password=qwerty12345";
+
+
+
+        public DataSet GetTable(string tableName)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var sql = $"SELECT * FROM {tableName}";
+                var adapter = new SqlDataAdapter(sql,connection);
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                return ds;
+            }
+        }
+
+        public IEnumerable<string> getTableNames()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = $"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+                var adapter = new SqlDataAdapter(sql,connection);
+                var ds = new DataSet();
+                adapter.Fill(ds);
+
+                return ParseTableNames(ds);
+            }
+        }
+
+        private IEnumerable<string> ParseTableNames(DataSet ds)
+        {
+            var names = new List<string>();
+            foreach (DataTable dataTable in ds.Tables)
+            {
+                names.Add(dataTable.TableName);
+            }
+
+            return names;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DBConection;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase dataBase = new DataBase();
+
+            var tableNames = dataBase.getTableNames();
+
+            foreach (var tableName in tableNames)
+            {
+                Console.WriteLine(tableName);
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}
