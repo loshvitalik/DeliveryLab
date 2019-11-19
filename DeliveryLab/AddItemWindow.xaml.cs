1b8f202e98c8c7d47eda7ff14edf1727d90f9736
loshvitalik <ban-ya-ru@ya.ru>
2019-11-20 03:10:19 +0500

    Item add window

---
+++ DeliveryLab/AddItemWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+namespace DeliveryLab
+{
+	/// <summary>
+	/// Логика взаимодействия для AddItemWindow.xaml
+	/// </summary>
+	public partial class AddItemWindow : Window
+	{
+		public AddItemWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void EnterKeyPress(object sender, KeyEventArgs e)
+		{
+			
+		}
+
+		private void AddButtonClick(object sender, RoutedEventArgs e)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
