25e61582f63c8e204755aff2c81b70c42d6e8708
loshvitalik <ban-ya-ru@ya.ru>
2019-11-20 02:37:27 +0500

    Update events

--- DBConection/DataBase.cs
+++ DBConection/DataBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -7,38 +6,44 @@ namespace DBConection
 {
     public class DataBase
     {
+		// строка подключения к БД
         private readonly string _connectionString = @"Data Source=localhost;Initial Catalog=udb_Аверьянов_Лощенко;User Id=sa; Password=1";
 
 
-
+	    // метод получения данных из таблицы по её названию
         public DataSet GetTable(string tableName)
         {
+			// запрос данных выполняется с использованием подключения с заданной строкой
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                var sql = $"SELECT * FROM {tableName}";
+                var sql = $"SELECT * FROM {tableName}"; // создаётся SQL-запрос SELECT, в который помещается переданное имя таблицы
                 var adapter = new SqlDataAdapter(sql,connection);
-                var ds = new DataSet();
-                adapter.Fill(ds);
+                var ds = new DataSet(); 
+                adapter.Fill(ds); // данные таблицы помещаются в класс DataSet и возвращаются
                 return ds;
             }
         }
 
+		// метод получения названий всех таблиц в БД
         public IEnumerable<string> getTableNames()
-        {
-            using (var connection = new SqlConnection(_connectionString))
+		{
+			// запрос данных выполняется с использованием подключения с заданной строкой
+			using (var connection = new SqlConnection(_connectionString))
             {
-                var sql = $"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+                var sql = $"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'"; // создаётся SQL-запрос SELECT из системной таблицы TABLES
                 var adapter = new SqlDataAdapter(sql,connection);
                 var ds = new DataSet();
-                adapter.Fill(ds);
+                adapter.Fill(ds); // данные таблицы помещаются в класс DataSet и возвращаются
 
-                return ParseTableNames(ds);
+				return ParseTableNames(ds);
             }
         }
 
+		// Метод выделения имён таблиц в виде строк из класса DataSet
         private IEnumerable<string> ParseTableNames(DataSet ds)
         {
-            var names = new List<string>();
+			var names = new List<string>();
+			// для каждой строки таблицы TABLES в список помещается имя таблицы, затем этот список возвращается
             foreach (DataRow dataRow in ds.Tables[0].Rows)
             {
                 names.Add(dataRow[2].ToString());
--- DeliveryLab/MainWindow.xaml.cs
+++ DeliveryLab/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,22 +19,24 @@ namespace DeliveryLab
 		public MainWindow()
 		{
 			InitializeComponent();
-			
 		}
 
+		// метод обработки изменения текущей активной таблицы
 		private void ShowTable(object sender, SelectionChangedEventArgs e)
 		{
-			var tableName = tableList.SelectedItem.ToString();
-			var data = dataBase.GetTable(tableName);
+			var tableName = tableList.SelectedItem.ToString(); // имя активной таблицы берётся из элемента ListBox
+			var data = dataBase.GetTable(tableName); // вызывается метод получения данных нужной таблицы
 			table.DataContext = data;
-			table.ItemsSource = data.Tables[0].DefaultView;
-			title.Content = tableName;
-			var columns = data.Tables[0].Columns;
-			var columnWidth = 100.0 / columns.Count;
-			table.Columns.Clear();
+			table.ItemsSource = data.Tables[0].DefaultView; // устанавливаются контекст и источник данных для элемента DataGrid
+			title.Content = tableName; // заголовок окна меняется на название выбранной таблицы
+			var columns = data.Tables[0].Columns; // из класса DataSet выделяется коллекция столбцов таблицы
+			var columnWidth = 100.0 / columns.Count; // все столбцы получают одинаковую равную ширину
+			table.Columns.Clear(); // элемент DataGrid очищается от предыдущих данных
+			// для каждого столбца из ранее полученного списка создаётся столбец в элементе DataGrid,
+			// заголовком столбца становится его название и выполоняется привязка к соответствующим данным
 			foreach (DataColumn column in columns)
 			{
-				if (column.DataType == typeof(bool))
+				if (column.DataType == typeof(bool)) // для логических значений БД создаётся CheckBoxColumn
 				{
 					table.Columns.Add(new DataGridCheckBoxColumn
 					{
@@ -41,7 +45,7 @@ namespace DeliveryLab
 						Width = new DataGridLength(columnWidth, DataGridLengthUnitType.Star)
 					});
 				}
-				else
+				else // для всех остальных - TextColumn
 				{
 					table.Columns.Add(new DataGridTextColumn
 					{
@@ -53,20 +57,36 @@ namespace DeliveryLab
 			}
 		}
 
+		// метод соединения с БД
 		private void ConnectButtonClick(object sender, RoutedEventArgs e)
 		{
-			dataBase = new DataBase();
-			var tableNames = dataBase.getTableNames();
-			tableList.ItemsSource = tableNames;
-			tableList.SelectedItem = tableNames.FirstOrDefault();
-
-			ShowTable("", null);
+			dataBase = new DataBase(); // создаётся новый экземпяр класса DataBase
+			var tableNames = dataBase.getTableNames(); // вызывается метод получения названий всех таблиц
+			tableList.ItemsSource = tableNames; // этот список названий помещается в элемент ListBox
+			connectButton.Content = "udb__Аверьянов__Лощенко"; // заголовок окна меняется на название базы данных
 		}
 
 		private void AboutButtonClick(object sender, RoutedEventArgs e)
 		{
 			new Alert("О программе \"Database Lab\"",
 				"Database Lab v. 2.0\n© 2019 loshvitalik, MrBlacktop").Show();
+		}
+
+		private void Table_OnCellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+		{
+			var name = title.Content.ToString();
+			var id = ((DataRowView)table.SelectedItem).Row.ItemArray[0];
+			var column = table.CurrentCell.Column.Header;
+			var data = ((TextBox)e.EditingElement).Text;
+			//dataBase.UpdateItem(name, id, column, data);
+		}
+
+		private void Table_OnCurrentCellChanged(object sender, EventArgs e)
+		{
+			var name = title.Content.ToString();
+			var id = ((DataRowView)table.SelectedItem).Row.ItemArray[0];
+			var column = table.CurrentCell.Column.Header;
+			//var data = ((TextBox)e.EditingElement).Text;
 		}
 	}
 }
